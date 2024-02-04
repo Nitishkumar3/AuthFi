4c7b3e4a48f953cf88948e942b205e517e0aabfd
Nitishkumar <nitishkumar.blog@gmail.com>
2024-02-04 11:52:22 +0530

    Added Encryption

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from pymongo import MongoClient
 import re
-from Modules import Auth
+from datetime import datetime
+
+from Modules import Auth, AES256
 
 app = Flask(__name__)
 app.secret_key = "GS1jv6dDu1hmVzdWySky7Me324VGPE6H4nMeXF3SsXZyEtRnTuh9y83tzQcQeC72"
@@ -20,15 +22,21 @@ def Index():
 @app.route('/register', methods=['GET', 'POST'])
 def Registration():
     if request.method == 'POST':
+        datecreated = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        name =  request.form['name']
         username = request.form['username']
-        password = request.form['password']
         email =  request.form['email']
+        password = request.form['password']
+        userid = AES256.GenerateRandomString()
 
+        while db.Users.find_one({'UserID': userid}):
+            userid = AES256.GenerateRandomString()
+    
         UserNameCheck = False if re.match(r'^[a-zA-Z0-9_]{4,}$', username) else True
         PasswordCheck = False if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-+=])[A-Za-z\d!@#$%^&*()-+=]{8,}$', password) else True
         ExistingUserName = True if db.Users.find_one({'UserName': username}) else False
         ExistingEmailID = True if db.Users.find_one({'Email': email}) else False
-        
+
         if UserNameCheck or PasswordCheck or ExistingUserName or ExistingEmailID:
             ErrorMessages = []
             if UserNameCheck:
@@ -42,8 +50,11 @@ def Registration():
             flash(ErrorMessages, 'error')
             return redirect(url_for('Registration'))
 
+        nameE = AES256.Encrypt(name, AES256.DeriveKey(userid, datecreated, "Name"))
+        passwordE = AES256.Encrypt(password, AES256.DeriveKey(userid, datecreated, "Password"))
+
         Auth.SendVerificationEmail(username, email, Auth.GenerateVerificationCode())
-        db.Users.insert_one({'UserName': username, 'Email': email, 'Password': password})
+        db.Users.insert_one({'UserID': userid, 'UserName': username, 'Name': nameE, 'Email': email, 'Password': passwordE, 'DateCreated': datecreated})
         return redirect(url_for('VerifyAccount', username=username))
     
     return render_template('register.html')
@@ -62,7 +73,7 @@ def VerifyAccount(username):
             db.UserVerification.update_one({'UserName': username}, {'$set': {'Verified': True}})
             return redirect(url_for('Login'))
         else:
-            flash('Invalid OTP. Please try again.', 'error')
+            flash('Invalid Code. Please try again.', 'error')
             return redirect(url_for('VerifyAccount', username=username))
 
     return render_template('verifyaccount.html', username=username)
@@ -83,7 +94,7 @@ def Login():
             flash('User not verified. Please complete the OTP verification.', 'error')
             return redirect(url_for('VerifyAccount', username=username))
 
-        if user and password == user["Password"]:
+        if user and password == AES256.Decrypt(user["Password"], AES256.DeriveKey(user["UserID"], user["DateCreated"], "Password")):
             session['username'] = username
             return redirect(url_for('Index'))
         else:
